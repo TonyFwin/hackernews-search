72bbbd0d8ad6eb97e2d6a98bf8dd52f5d855dc50
Tony Nguyen <tony.nguyen@epicosity.com>
2019-04-10 12:40:06 -0500

    Add state and dismiss function

--- src/App.js
+++ src/App.js
@@ -22,10 +22,26 @@ const list = [
 ];
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      list
+    };
+
+    this.onDimiss = this.onDimiss.bind(this);
+  }
+
+  onDimiss(id) {
+    const isNotId = item => item.objectID !== id;
+    const updatedList = this.state.list.filter(isNotId);
+    this.setState({ list: updatedList });
+  }
+
   render() {
     return (
       <div className="App">
-        {list.map(item => (
+        {this.state.list.map(item => (
           <div key={item.objectID}>
             <span>
               <a href={item.url}>{item.title}</a>
@@ -33,6 +49,14 @@ class App extends Component {
             <span>{item.author}</span>
             <span>{item.num_comments}</span>
             <span>{item.points}</span>
+            <span>
+              <button
+                onClick={() => this.onDimiss(item.objectID)}
+                type="button"
+              >
+                Dismiss
+              </button>
+            </span>
           </div>
         ))}
       </div>
