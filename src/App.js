530336f242db98f2136c255dcf6d13b605fdaedd
Tony Nguyen <tony.nguyen@epicosity.com>
2019-04-17 08:33:06 -0500

    Convert input into controlled component

--- src/App.js
+++ src/App.js
@@ -59,12 +59,13 @@ class App extends Component {
   }
 
   render() {
+    const { searchTerm, list } = this.state;
     return (
       <div className="App">
         <form>
-          <input type="text" onChange={this.onSearchChange} />
+          <input type="text" onChange={this.onSearchChange} value={searchTerm} />
         </form>
-        {this.state.list.filter(isSearched(this.state.searchTerm)).map(item => (
+        {list.filter(isSearched(searchTerm)).map(item => (
           <div key={item.objectID}>
             <span>
               <a href={item.url}>{item.title}</a>
