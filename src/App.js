0f0061b86a309180b24438fb91d21c05d299e6b2
Tony Nguyen <tony.nguyen@epicosity.com>
2019-08-27 21:04:38 -0500

    implement data fetching

--- src/App.js
+++ src/App.js
@@ -1,44 +1,22 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
 import './App.css';
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1
-  },
-  {
-    title: 'GraphQL',
-    url: 'https://graphql.org/',
-    author: 'Angel Gomez',
-    num_comments: 1,
-    points: 3,
-    objectID: 2
-  }
-];
+const DEFAULT_QUERY = 'redux';
+
+const PATH_BASE = 'https://hn.algolia.com/api/v1';
+const PATH_SEARCH = '/search';
+const PARAM_SEARCH = 'query=';
 
 const largeColumn = {
-  width: '40%'
+  width: '40%',
 };
 
 const midColumn = {
-  width: '30%'
+  width: '30%',
 };
 
 const smallColumn = {
-  width: '10%'
+  width: '10%',
 };
 
 const isSearched = searchTerm => item =>
@@ -49,12 +27,26 @@ class App extends Component {
     super(props);
 
     this.state = {
-      list,
-      searchTerm: ''
+      result: null,
+      searchTerm: DEFAULT_QUERY,
     };
 
+    this.setSearchTopStories = this.setSearchTopStories.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
     this.onDismiss = this.onDismiss.bind(this);
+  }
+
+  setSearchTopStories(result) {
+    this.setState({ result });
+  }
+
+  componentDidMount() {
+    const { searchTerm } = this.state;
+
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+      .then(response => response.json())
+      .then(result => this.setSearchTopStories(result))
+      .catch(error => error);
   }
 
   onSearchChange(event) {
@@ -68,36 +60,55 @@ class App extends Component {
   }
 
   render() {
-    const { searchTerm, list } = this.state;
+    const { searchTerm, result } = this.state;
+
+    if (!result) { return null; }
+
     return (
       <div className="page">
         <div className="interactions">
-          <Search value={searchTerm} onChange={this.onSearchChange}>
+          <Search
+            value={searchTerm}
+            onChange={this.onSearchChange}
+          >
             Search
           </Search>
         </div>
-        <Table list={list} pattern={searchTerm} onDismiss={this.onDismiss} />
+        <Table
+          list={result.hits}
+          pattern={searchTerm}
+          onDismiss={this.onDismiss}
+        />
       </div>
     );
   }
 }
 
-const Search = ({ value, onChange, children }) => (
+const Search = ({ value, onChange, children }) =>
   <form>
-    {children} <input type="text" value={value} onChange={onChange} />
+    {children} <input
+      type="text"
+      value={value}
+      onChange={onChange}
+    />
   </form>
-);
 
-const Table = ({ list, pattern, onDismiss }) => (
+const Table = ({ list, pattern, onDismiss }) =>
   <div className="table">
-    {list.filter(isSearched(pattern)).map(item => (
+    {list.filter(isSearched(pattern)).map(item =>
       <div key={item.objectID} className="table-row">
         <span style={largeColumn}>
           <a href={item.url}>{item.title}</a>
         </span>
-        <span style={midColumn}>{item.author}</span>
-        <span style={smallColumn}>{item.num_comments}</span>
-        <span style={smallColumn}>{item.points}</span>
+        <span style={midColumn}>
+          {item.author}
+        </span>
+        <span style={smallColumn}>
+          {item.num_comments}
+        </span>
+        <span style={smallColumn}>
+          {item.points}
+        </span>
         <span style={smallColumn}>
           <Button
             onClick={() => onDismiss(item.objectID)}
@@ -107,14 +118,21 @@ const Table = ({ list, pattern, onDismiss }) => (
           </Button>
         </span>
       </div>
-    ))}
+    )}
   </div>
-);
 
-const Button = ({ onClick, className = '', children }) => (
-  <button onClick={onClick} className={className} type="button">
+
+const Button = ({
+  onClick,
+  className = '',
+  children,
+}) =>
+  <button
+    onClick={onClick}
+    className={className}
+    type="button"
+  >
     {children}
   </button>
-);
 
 export default App;
