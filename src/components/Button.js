436d6ba2d9abfc6a3328aaa02304c1f5f18d9aad
Tony Nguyen <tony.nguyen@epicosity.com>
2019-09-09 21:51:27 -0500

    add type checkign with prop types to button and table

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,15 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Button = ({ onClick, className = '', children}) => {
-  return (
-    <button
-      onClick={onClick}
-      className={className}
-      type="button"
-    >
-      {children}
-    </button>
-  )
-}
+const Button = ({
+  onClick,
+  className,
+  children,
+}) =>
+  <button
+    onClick={onClick}
+    className={className}
+    type="button"
+  >
+    {children}
+  </button>
+
+Button.defaultProps = {
+  className: '',
+};
+
+Button.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  className: PropTypes.string,
+  children: PropTypes.node.isRequired,
+};
+
 
 export default Button;
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import Button from './Button'
+import Button from './Button';
+import PropTypes from 'prop-types';
 
 const largeColumn = {
   width: '40%',
@@ -43,6 +44,20 @@ const Table = ({ list, onDismiss }) => {
       )}
     </div>
   )
+}
+
+
+Table.propTypes = {
+  list: PropTypes.arrayOf(
+    PropTypes.shape({
+      objectID: PropTypes.string.isRequired,
+      author: PropTypes.string,
+      url: PropTypes.string,
+      num_comments: PropTypes.number,
+      points: PropTypes.number,
+    })
+  ).isRequired,
+  onDismiss: PropTypes.func.isRequired
 }
 
 export default Table;
