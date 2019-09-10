12e240fd8e6141be78255061d5c41275495514f4
Tony Nguyen <tony.nguyen@epicosity.com>
2019-09-09 21:58:37 -0500

    use ref attribute to add focus to search input

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Search = ({ value, onChange, onSubmit, children}) => {
-  return (
-    <form onSubmit={onSubmit}>
-      <input
-        type="text"
-        value={value}
-        onChange={onChange}
-      />
-      <button type="submit">
-        {children}
-      </button>
-    </form>
-  )
+class Search extends Component {
+  componentDidMount() {
+    if (this.input) {
+      this.input.focus();
+    }
+  }
+  
+  render(){
+    const { value, onChange, onSubmit, children } = this.props;
+    return (
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          value={value}
+          onChange={onChange}
+          ref={el => this.input = el}
+        />
+        <button type="submit">
+          {children}
+        </button>
+      </form>
+    )
+  }
 }
 
 export default Search;
